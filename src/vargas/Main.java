5dc0b08475c7c315c32d506b62818ad5c02f6841
jonjaira <jonjaira@gmail.com>
2020-05-12 12:44:03 -0400

    Initial commit

---
+++ src/vargas/Main.java
@@ -0,0 +1,12 @@
+package vargas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write a program to introduce yourself
+    String name = "Jessica";
+
+    System.out.println("Hello my name is: "+ name);
+
+    }
+}
