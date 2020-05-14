efeaa43e585c7432589d4ca892478baa0f173e6c
jonjaira <jonjaira@gmail.com>
2020-05-13 21:12:59 -0400

    First change

--- src/vargas/Main.java
+++ src/vargas/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write a program to introduce yourself
-    String name = "Jessica";
+    String name = "Jessica Vargas";
 
     System.out.println("Hello my name is: "+ name);
 
