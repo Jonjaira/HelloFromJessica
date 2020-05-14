a7c7ec24380f25846670e4b451eae8d2b6a69247
jonjaira <jonjaira@gmail.com>
2020-05-14 17:15:57 -0400

    Undo changes test

--- src/vargas/Main.java
+++ src/vargas/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write a program to introduce yourself
     String name = "Taylor";
-// hello this is taylor
-    System.out.println("Hello my name is: "+ name);
+    String me = "Jessica";
+    // hello this is taylor
+    System.out.println("Hello my name is: "+ name + "Hello " + name + "I'm " + me);
 
     System.out.println("Hello Jessica, my name is Taylor");
 
